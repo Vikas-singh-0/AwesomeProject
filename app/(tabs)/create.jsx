608d76c83aa7faef410b506d0258a486b7bff129
Vikas Singh <s.vikas892354@gmail.com>
2024-05-06 23:52:04 +0530

    minor changes

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,14 +1,52 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import FormField from "../../components/FormField";
+import { Video } from "expo-av";
 
-const create = () => {
+const Create = () => {
+  const [uploading, setUploading] = useState(false);
+  const [form, setForm] = useState({
+    title: "",
+    video: null,
+    thumbnail: null,
+    prompt: "",
+  });
   return (
-    <View>
-      <Text>create</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView className="px-4 my-6">
+        <Text className="text-2xl text-white font-semibold">Upload VIdeo</Text>
+        <FormField
+          title="Video Title"
+          value={form.title}
+          handleChangeText={(e) => setForm({ ...form, title: e })}
+          otherStyles="mt-10"
+        />
+        <View className="mt-7 space-y-2">
+          <Text className="text-base text-grey-100 font-pmedium">
+            Upload Video
+          </Text>
+          <TouchableOpacity>
+            {form.video ? (
+              <Video></Video>
+            ) : (
+              <View className="w-full h-40 px-4 bg-black-200 rounded-2xl justify-center items-center">
+                <View className="w-14 h-14 border border-dashed ">
 
-export default create
+                </View>
+              </View>
+            )}
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default Create;
